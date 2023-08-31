e6dffabd6abd23bbe9f5d9f6545008279c3ec360
Vincenza-Genuardi <genuardi.vincenza@iisgalvanimi.edu.it>
2023-08-31 10:02:12 +0000

    ordinamento dati in base al prezzo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { SpartitiStrumenti } from './models/SpartitiStrumenti.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Strumento } from './models/strumento.model';
+import { Spartito } from './models/spartito.model';
 
 @Component({
   selector: 'app-root',
@@ -9,16 +11,23 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  data !: SpartitiStrumenti
-  URL:string='https://my-json-server.typicode.com/Vincenza-Genuardi/StrumentiESpartiti/db'
-  OStreaming!: Observable<SpartitiStrumenti>
-   
+  data !: SpartitiStrumenti;
+  URL:string='https://my-json-server.typicode.com/Vincenza-Genuardi/StrumentiESpartiti/db';
+  OStreaming!: Observable<SpartitiStrumenti>;
+
   constructor(public http: HttpClient) {
     this.makeTypedRequest()
-  
+    
   }
   makeTypedRequest():void{
     this.OStreaming = this.http.get<SpartitiStrumenti>(this.URL);
-    this.OStreaming.subscribe( d => {this.data = d;});
+    this.OStreaming.subscribe( d => {this.data = d; this.ordinamento();});
+   
   }
+  ordinamento():boolean{
+    this.data.spartiti.sort((a: Spartito, b: Spartito) => a.prezzo-b.prezzo );
+    this.data.strumenti.sort((a: Strumento, b: Strumento) => a.prezzo-b.prezzo );
+    return false;
+  }
+  
 }
